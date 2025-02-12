7fb1e9bcb2b63b9f09d16f8e4d09bedc8c455157
arbazarshad7 <65922316+arbazarshad7@users.noreply.github.com>
2025-02-12 08:03:29 +0000

    day1

--- MyJS/test.js
+++ MyJS/test.js
@@ -1,1 +1,14 @@
 console.log("hello")
+const accountID = 12536
+let accountEmail = "arbazarshad7@gmail.com"
+var accountPasswrod = "1234"
+accountCity ="Jaipur"
+let accountState;
+
+// accountID =2 // not allowed
+
+accountEmail = "hc#hc"
+accountPasswrod = "123321"
+accountCity = "Delhi"
+
+console.table([accountID, accountEmail, accountPasswrod, accountCity, accountState]);
